36611c704aff596655beed6de16d68eccb2d6761
Lavolin <lavolin@gmail.com>
2022-08-30 23:06:26 +0300

    Add project files.

---
+++ FastFood.Core/Controllers/CategoriesController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using AutoMapper;
+using FastFood.Data;
+using FastFood.Web.ViewModels.Categories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FastFood.Web.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private readonly FastFoodContext context;
+        private readonly IMapper mapper;
+
+        public CategoriesController(FastFoodContext context, IMapper mapper)
+        {
+            this.context = context;
+            this.mapper = mapper;
+        }
+
+        public IActionResult Create()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(CreateCategoryInputModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IActionResult All()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ FastFood.Core/Controllers/ItemsController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using AutoMapper;
+using FastFood.Data;
+using FastFood.Web.ViewModels.Items;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FastFood.Web.Controllers
+{
+    public class ItemsController : Controller
+    {
+        private readonly FastFoodContext context;
+        private readonly IMapper mapper;
+
+        public ItemsController(FastFoodContext context, IMapper mapper)
+        {
+            this.context = context;
+            this.mapper = mapper;
+        }
+
+        public IActionResult Create()
+        {
+            var categories = this.context.Categories
+                .ProjectTo<CreateItemViewModel>(mapper.ConfigurationProvider)
+                .ToList();
+
+            return this.View(categories);
+        }
+
+        [HttpPost]
+        public IActionResult Create(CreateItemInputModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IActionResult All()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
